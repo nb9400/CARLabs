86a911ee3257051f4c8d1b3fe2270550fdf1c8e6
NachiBarve <nachiketa.barve@gmail.com>
2021-10-01 13:53:54 +0530

    Removed trigonometry function and added default case.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,43 +1,38 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include<iostream>
 #include<math.h>
 
 using namespace std;
 
 int logarithm();
-int trig();
 int power();
-
-
 
 int main()
 {   
     int a;
     cout<<"Calculator-";
     cout<<"\n1. Logarithm";
-    cout<<"\n2. Trigonometric fn";
-    cout<<"\n3. Power";
+    cout<<"\n2. Power";
     cout<<"\nEnter option-";
     cin>>a;
     
     switch(a)
     {
-        case 1: logarithm();
-    
-        case 2: trig();
-        
-        case 3: power();
-        
-        //case default:cout<<"Invalid option";
+        case 1:
+        {	
+	logarithm();
+	break;
+        }    
+        case 2:
+       {
+	power();
+	break;
+        }        
+        default:
+       {
+	cout<<"Invalid option";
+	break;
+        }
     }
-
     return 0;
 }
 
@@ -51,40 +46,10 @@ int logarithm()
     cout<<c;
 }
 
-int trig()
-{
-    float a,b,c,d,hyp,f;
-    int e;
-    cout<<"\nDo you have angle(1) or sides(2)-";
-    cin>>e;
-    if(e==1)
-    {
-        cout<<"Enter angle in degree";
-        cin>>a;
-        a=((22/7)*a)/180;
-        b=sin(a);
-        cout<<b;
-    }
-    else
-    {
-        cout<<"Enter base and perpendicular length";
-        cin>>c>>d;
-        hyp=sqrt(((c)*(c))+(d*d));
-        f=d/hyp;
-        cout<<"Sin value-"<<f;
-    
-        cout<<"cos value-"<<sqrt(1-(f*f));
-        
-    }
-    
-}
-
 int power()
 {
     float x,y;
     cout<<"Enter 2 nos";
     cin>>x>>y;
     cout<<pow(x,y);
-    
-    
 }
